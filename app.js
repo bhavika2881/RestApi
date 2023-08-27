8f0d0fe53b04837c3525afcbec15caf6ce0265cb
bhavika <bhavikamegh2881@gamil.com>
2023-08-27 14:17:43 +0530

    Updated index.html and added styles.css

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs').promises;
 const path = require('path');
+const mime = require('mime-types'); 
 
 const app = express();
 const port = 5500;
 
-// Set up static file serving
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const storage = multer.diskStorage({
   destination: 'uploads/',
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original name of the file
+    cb(null, file.originalname); 
   },
 });
 
 const upload = multer({ storage });
 const cors = require('cors');
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', upload.single('file'), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded.' });
   }
@@ -28,23 +29,28 @@ app.post('/upload', upload.single('file'), (req, res) => {
     size: req.file.size,
     destination: req.file.destination,
     filename: req.file.filename,
-    path: req.file.path
+    path: req.file.path,
   };
+
+ 
+  const detectedType = mime.lookup(fileInfo.filename);
+  fileInfo.fileType = detectedType || 'unknown';
 
   res.json(fileInfo);
 });
 
 app.get('/files', async (req, res) => {
   try {
-    console.log("Request received for /files"); 
     const files = await fs.readdir(path.join(__dirname, 'uploads'));
     const fileInfos = await Promise.all(files.map(async (filename) => {
       const filePath = path.join(__dirname, 'uploads', filename);
       const fileStat = await fs.stat(filePath);
+      const detectedType = mime.lookup(filename); 
       return {
         filename: filename,
         size: fileStat.size,
-        path: filePath
+        path: filePath,
+        fileType: detectedType || 'unknown' 
       };
     }));
     res.json(fileInfos);
@@ -53,21 +59,25 @@ app.get('/files', async (req, res) => {
   }
 });
 
+
 app.get('/file/:filename', async (req, res) => {
   const { filename } = req.params;
   const filePath = path.join(__dirname, 'uploads', filename);
 
   try {
     const fileStat = await fs.stat(filePath);
+    const detectedType = mime.lookup(filename); 
     res.json({
       filename: filename,
       size: fileStat.size,
-      path: filePath
+      path: filePath,
+      fileType: detectedType || 'unknown' 
     });
   } catch (error) {
     res.status(404).json({ error: 'File not found.' });
   }
 });
+
 
 app.delete('/file/:filename', async (req, res) => {
   const { filename } = req.params;
